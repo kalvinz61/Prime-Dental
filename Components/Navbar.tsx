fefc3e0eb29d0ea65435ad2c8bb7850fd2712424
Kalvin <54190452+kalvinz61@users.noreply.github.com>
2023-03-29 14:47:29 -0400

    22 add styles (#23)
    * added styles
    
    * added styles to navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,3 +1,28 @@
+import styles from './Navbar.module.scss';
+import Link from 'next/link';
+
 export default function Navbar() {
-    return <div>NAVBAR</div>
+  return (
+    <div className={styles.nav}>
+      <div className={styles.home}>
+        <Link href="/">
+          <h1 className={styles.link}>Prime Dental</h1>
+        </Link>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <Link href="/technology">
+            <h2 className={styles.link}>Technology</h2>
+          </Link>
+        </div>
+
+        <div className={styles.item}>
+          <Link href="/turnaround">
+            <h2 className={styles.link}>Turnaround Times</h2>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,203 @@
 export default function Home() {
-	return <h1>HOME PAGE</h1>;
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+      <p>
+        Lorem Ipsum "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet, consectetur, adipisci velit..." "There is no one who loves pain
+        itself, who seeks after it and wants to have it, simply because it is
+        pain..." Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        elementum dictum convallis. Nunc non erat aliquam, ullamcorper sapien
+        et, tincidunt nisl. Sed ultrices velit orci, vitae malesuada libero
+        iaculis non. Fusce ipsum turpis, sollicitudin ac ultricies mollis,
+        ullamcorper nec ex. In augue diam, commodo ac molestie a, condimentum id
+        nunc. Nam aliquam consequat iaculis. Integer non leo a magna mattis
+        rhoncus eget venenatis turpis. Morbi maximus purus ut mollis sodales.
+        Etiam ut auctor eros, lacinia scelerisque arcu. Duis ornare ornare
+        sollicitudin. Donec sed venenatis mi, et congue nunc. Suspendisse id
+        velit feugiat ante sollicitudin lacinia. Etiam cursus tincidunt porta.
+        Morbi vel elit finibus, pulvinar urna vel, tincidunt eros. Sed blandit
+        neque dui, at tempus leo commodo a. Nulla aliquam, lectus quis pulvinar
+        fringilla, nunc velit ultrices ante, sed consectetur neque dui sit amet
+        mi. In ut eros justo. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Nunc est velit, pulvinar ut tellus quis, mollis tincidunt
+        lacus. Nam eu dolor blandit, malesuada magna a, lobortis nisl. Vivamus
+        sed volutpat est. Curabitur vulputate elit id felis aliquam fermentum.
+        Donec tellus eros, malesuada rhoncus tortor at, tincidunt feugiat lacus.
+        Etiam eget diam vel odio mattis maximus vel ut purus. Suspendisse
+        potenti. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Integer tempus magna non lacus
+        ullamcorper, id vulputate velit vehicula. Duis vestibulum velit sed
+        lorem cursus pellentesque. Pellentesque porta venenatis neque, sit amet
+        consectetur nibh porta in. Sed vel dolor cursus, vulputate leo in,
+        dictum quam. Pellentesque ut justo ut leo hendrerit dapibus. Vestibulum
+        ullamcorper, est at auctor rutrum, eros est ornare tellus, et
+        condimentum nulla nunc dapibus nisi. Nunc accumsan odio nec felis
+        gravida, eget fringilla enim pellentesque. Nunc egestas orci ex, eget
+        condimentum mi eleifend non. Aenean lobortis malesuada tempor. Maecenas
+        imperdiet malesuada purus, vel consequat neque vulputate a. Phasellus
+        quis enim ultricies erat porta elementum. Quisque quis lacus a ex
+        condimentum luctus id non nisl. Integer quis suscipit purus, nec
+        molestie leo. Aliquam ultricies mattis dui, non feugiat dolor. Donec
+        accumsan libero fringilla metus vestibulum, quis luctus arcu dapibus.
+        Donec mattis eleifend lorem sit amet egestas. Nullam sodales in magna ac
+        tincidunt. Sed euismod tortor ut lectus elementum eleifend. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Etiam consectetur eros non tempus venenatis. Aenean fermentum
+        finibus orci, non interdum tortor consectetur in. Nullam sed leo
+        finibus, varius urna id, dignissim elit. Maecenas purus ante, semper at
+        posuere ut, aliquet at orci. Vivamus lorem turpis, lacinia sit amet
+        nulla quis, tempor porta sapien. Nunc sed tellus vel risus dapibus
+        tristique. In hac habitasse platea dictumst. Proin commodo scelerisque
+        nulla ac imperdiet. Vivamus quis dictum leo.
+      </p>
+      <p>
+        Lorem Ipsum "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet, consectetur, adipisci velit..." "There is no one who loves pain
+        itself, who seeks after it and wants to have it, simply because it is
+        pain..." Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        elementum dictum convallis. Nunc non erat aliquam, ullamcorper sapien
+        et, tincidunt nisl. Sed ultrices velit orci, vitae malesuada libero
+        iaculis non. Fusce ipsum turpis, sollicitudin ac ultricies mollis,
+        ullamcorper nec ex. In augue diam, commodo ac molestie a, condimentum id
+        nunc. Nam aliquam consequat iaculis. Integer non leo a magna mattis
+        rhoncus eget venenatis turpis. Morbi maximus purus ut mollis sodales.
+        Etiam ut auctor eros, lacinia scelerisque arcu. Duis ornare ornare
+        sollicitudin. Donec sed venenatis mi, et congue nunc. Suspendisse id
+        velit feugiat ante sollicitudin lacinia. Etiam cursus tincidunt porta.
+        Morbi vel elit finibus, pulvinar urna vel, tincidunt eros. Sed blandit
+        neque dui, at tempus leo commodo a. Nulla aliquam, lectus quis pulvinar
+        fringilla, nunc velit ultrices ante, sed consectetur neque dui sit amet
+        mi. In ut eros justo. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Nunc est velit, pulvinar ut tellus quis, mollis tincidunt
+        lacus. Nam eu dolor blandit, malesuada magna a, lobortis nisl. Vivamus
+        sed volutpat est. Curabitur vulputate elit id felis aliquam fermentum.
+        Donec tellus eros, malesuada rhoncus tortor at, tincidunt feugiat lacus.
+        Etiam eget diam vel odio mattis maximus vel ut purus. Suspendisse
+        potenti. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Integer tempus magna non lacus
+        ullamcorper, id vulputate velit vehicula. Duis vestibulum velit sed
+        lorem cursus pellentesque. Pellentesque porta venenatis neque, sit amet
+        consectetur nibh porta in. Sed vel dolor cursus, vulputate leo in,
+        dictum quam. Pellentesque ut justo ut leo hendrerit dapibus. Vestibulum
+        ullamcorper, est at auctor rutrum, eros est ornare tellus, et
+        condimentum nulla nunc dapibus nisi. Nunc accumsan odio nec felis
+        gravida, eget fringilla enim pellentesque. Nunc egestas orci ex, eget
+        condimentum mi eleifend non. Aenean lobortis malesuada tempor. Maecenas
+        imperdiet malesuada purus, vel consequat neque vulputate a. Phasellus
+        quis enim ultricies erat porta elementum. Quisque quis lacus a ex
+        condimentum luctus id non nisl. Integer quis suscipit purus, nec
+        molestie leo. Aliquam ultricies mattis dui, non feugiat dolor. Donec
+        accumsan libero fringilla metus vestibulum, quis luctus arcu dapibus.
+        Donec mattis eleifend lorem sit amet egestas. Nullam sodales in magna ac
+        tincidunt. Sed euismod tortor ut lectus elementum eleifend. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Etiam consectetur eros non tempus venenatis. Aenean fermentum
+        finibus orci, non interdum tortor consectetur in. Nullam sed leo
+        finibus, varius urna id, dignissim elit. Maecenas purus ante, semper at
+        posuere ut, aliquet at orci. Vivamus lorem turpis, lacinia sit amet
+        nulla quis, tempor porta sapien. Nunc sed tellus vel risus dapibus
+        tristique. In hac habitasse platea dictumst. Proin commodo scelerisque
+        nulla ac imperdiet. Vivamus quis dictum leo.
+      </p>
+      <p>
+        Lorem Ipsum "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet, consectetur, adipisci velit..." "There is no one who loves pain
+        itself, who seeks after it and wants to have it, simply because it is
+        pain..." Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        elementum dictum convallis. Nunc non erat aliquam, ullamcorper sapien
+        et, tincidunt nisl. Sed ultrices velit orci, vitae malesuada libero
+        iaculis non. Fusce ipsum turpis, sollicitudin ac ultricies mollis,
+        ullamcorper nec ex. In augue diam, commodo ac molestie a, condimentum id
+        nunc. Nam aliquam consequat iaculis. Integer non leo a magna mattis
+        rhoncus eget venenatis turpis. Morbi maximus purus ut mollis sodales.
+        Etiam ut auctor eros, lacinia scelerisque arcu. Duis ornare ornare
+        sollicitudin. Donec sed venenatis mi, et congue nunc. Suspendisse id
+        velit feugiat ante sollicitudin lacinia. Etiam cursus tincidunt porta.
+        Morbi vel elit finibus, pulvinar urna vel, tincidunt eros. Sed blandit
+        neque dui, at tempus leo commodo a. Nulla aliquam, lectus quis pulvinar
+        fringilla, nunc velit ultrices ante, sed consectetur neque dui sit amet
+        mi. In ut eros justo. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Nunc est velit, pulvinar ut tellus quis, mollis tincidunt
+        lacus. Nam eu dolor blandit, malesuada magna a, lobortis nisl. Vivamus
+        sed volutpat est. Curabitur vulputate elit id felis aliquam fermentum.
+        Donec tellus eros, malesuada rhoncus tortor at, tincidunt feugiat lacus.
+        Etiam eget diam vel odio mattis maximus vel ut purus. Suspendisse
+        potenti. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Integer tempus magna non lacus
+        ullamcorper, id vulputate velit vehicula. Duis vestibulum velit sed
+        lorem cursus pellentesque. Pellentesque porta venenatis neque, sit amet
+        consectetur nibh porta in. Sed vel dolor cursus, vulputate leo in,
+        dictum quam. Pellentesque ut justo ut leo hendrerit dapibus. Vestibulum
+        ullamcorper, est at auctor rutrum, eros est ornare tellus, et
+        condimentum nulla nunc dapibus nisi. Nunc accumsan odio nec felis
+        gravida, eget fringilla enim pellentesque. Nunc egestas orci ex, eget
+        condimentum mi eleifend non. Aenean lobortis malesuada tempor. Maecenas
+        imperdiet malesuada purus, vel consequat neque vulputate a. Phasellus
+        quis enim ultricies erat porta elementum. Quisque quis lacus a ex
+        condimentum luctus id non nisl. Integer quis suscipit purus, nec
+        molestie leo. Aliquam ultricies mattis dui, non feugiat dolor. Donec
+        accumsan libero fringilla metus vestibulum, quis luctus arcu dapibus.
+        Donec mattis eleifend lorem sit amet egestas. Nullam sodales in magna ac
+        tincidunt. Sed euismod tortor ut lectus elementum eleifend. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Etiam consectetur eros non tempus venenatis. Aenean fermentum
+        finibus orci, non interdum tortor consectetur in. Nullam sed leo
+        finibus, varius urna id, dignissim elit. Maecenas purus ante, semper at
+        posuere ut, aliquet at orci. Vivamus lorem turpis, lacinia sit amet
+        nulla quis, tempor porta sapien. Nunc sed tellus vel risus dapibus
+        tristique. In hac habitasse platea dictumst. Proin commodo scelerisque
+        nulla ac imperdiet. Vivamus quis dictum leo.
+      </p>
+      <p>
+        Lorem Ipsum "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet, consectetur, adipisci velit..." "There is no one who loves pain
+        itself, who seeks after it and wants to have it, simply because it is
+        pain..." Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        elementum dictum convallis. Nunc non erat aliquam, ullamcorper sapien
+        et, tincidunt nisl. Sed ultrices velit orci, vitae malesuada libero
+        iaculis non. Fusce ipsum turpis, sollicitudin ac ultricies mollis,
+        ullamcorper nec ex. In augue diam, commodo ac molestie a, condimentum id
+        nunc. Nam aliquam consequat iaculis. Integer non leo a magna mattis
+        rhoncus eget venenatis turpis. Morbi maximus purus ut mollis sodales.
+        Etiam ut auctor eros, lacinia scelerisque arcu. Duis ornare ornare
+        sollicitudin. Donec sed venenatis mi, et congue nunc. Suspendisse id
+        velit feugiat ante sollicitudin lacinia. Etiam cursus tincidunt porta.
+        Morbi vel elit finibus, pulvinar urna vel, tincidunt eros. Sed blandit
+        neque dui, at tempus leo commodo a. Nulla aliquam, lectus quis pulvinar
+        fringilla, nunc velit ultrices ante, sed consectetur neque dui sit amet
+        mi. In ut eros justo. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Nunc est velit, pulvinar ut tellus quis, mollis tincidunt
+        lacus. Nam eu dolor blandit, malesuada magna a, lobortis nisl. Vivamus
+        sed volutpat est. Curabitur vulputate elit id felis aliquam fermentum.
+        Donec tellus eros, malesuada rhoncus tortor at, tincidunt feugiat lacus.
+        Etiam eget diam vel odio mattis maximus vel ut purus. Suspendisse
+        potenti. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Integer tempus magna non lacus
+        ullamcorper, id vulputate velit vehicula. Duis vestibulum velit sed
+        lorem cursus pellentesque. Pellentesque porta venenatis neque, sit amet
+        consectetur nibh porta in. Sed vel dolor cursus, vulputate leo in,
+        dictum quam. Pellentesque ut justo ut leo hendrerit dapibus. Vestibulum
+        ullamcorper, est at auctor rutrum, eros est ornare tellus, et
+        condimentum nulla nunc dapibus nisi. Nunc accumsan odio nec felis
+        gravida, eget fringilla enim pellentesque. Nunc egestas orci ex, eget
+        condimentum mi eleifend non. Aenean lobortis malesuada tempor. Maecenas
+        imperdiet malesuada purus, vel consequat neque vulputate a. Phasellus
+        quis enim ultricies erat porta elementum. Quisque quis lacus a ex
+        condimentum luctus id non nisl. Integer quis suscipit purus, nec
+        molestie leo. Aliquam ultricies mattis dui, non feugiat dolor. Donec
+        accumsan libero fringilla metus vestibulum, quis luctus arcu dapibus.
+        Donec mattis eleifend lorem sit amet egestas. Nullam sodales in magna ac
+        tincidunt. Sed euismod tortor ut lectus elementum eleifend. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Etiam consectetur eros non tempus venenatis. Aenean fermentum
+        finibus orci, non interdum tortor consectetur in. Nullam sed leo
+        finibus, varius urna id, dignissim elit. Maecenas purus ante, semper at
+        posuere ut, aliquet at orci. Vivamus lorem turpis, lacinia sit amet
+        nulla quis, tempor porta sapien. Nunc sed tellus vel risus dapibus
+        tristique. In hac habitasse platea dictumst. Proin commodo scelerisque
+        nulla ac imperdiet. Vivamus quis dictum leo.
+      </p>
+    </div>
+  );
 }
