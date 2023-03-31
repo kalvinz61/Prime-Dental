3d1e9d6f356f8bdc66b3596711195d9749686e73
Kalvin <kalvinz61@gmail.com>
2023-03-31 17:11:30 -0400

    styled and added cards to home page

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,5 +1,5 @@
-import styles from './Footer.module.scss';
+import styles from '../styles/modules/Footer.module.scss';
 
 export default function Footer() {
-  return <div className={styles.footer}>Hello</div>;
+  return <div className={styles.footer}>Footer</div>;
 }
